fba290de6907b90f502b2f22adb1f88358fd313f
arvin-mf <arvinmulia55@gmail.com>
2024-09-09 01:36:50 +0700

    feat(category): complete seeder

--- internal/db/queries/category.sql
+++ internal/db/queries/category.sql
@@ -1,9 +1,22 @@
 -- name: AddCategory :execresult
-INSERT INTO categories (id, category_name)
-VALUES (?, ?);
+INSERT INTO categories (id, category_name, group_id)
+VALUES (?, ?, ?);
+
+-- name: AddCategoryGroup :execresult
+INSERT INTO category_groups (group_name)
+VALUES (?);
+
+-- name: CategoryExists :one
+SELECT COUNT(1) FROM categories WHERE category_name = ? AND group_id = ?;
+
+-- name: CategoryGroupExists :one
+SELECT COUNT(1) FROM category_groups WHERE group_name = ?;
+
+-- name: GetCategoryGroupIDByName :one
+SELECT id FROM category_groups WHERE group_name = ?;
 
 -- name: UserCategoryExists :one
-SELECT COUNT(1) from user_categories
+SELECT COUNT(1) FROM user_categories
 WHERE category_id = ? AND user_id = ?;
 
 -- name: CreateUserCategory :execresult
@@ -14,6 +27,6 @@ VALUES (?, ?);
 SELECT * FROM categories;
 
 -- name: GetCategoriesForUser :many
-SELECT categories.id, category_name, categories.created_at FROM categories
+SELECT categories.id, categories.created_at, category_name, group_id FROM categories
 LEFT JOIN user_categories ON categories.id = category_id
 WHERE user_id = ?;
--- internal/seeder/categories_seeder.go
+++ internal/seeder/categories_seeder.go
@@ -24,18 +24,87 @@ func main() {
 	queries := sqlc.New(db)
 	repo := repository.NewSeederRepository(queries)
 
+	groups := []string{"College", "Work", "Community"}
+
+	for _, group := range groups {
+		exist, err := repo.CategoryGroupExists(group)
+		if err != nil {
+			log.Fatalf("Failed checking category group: %v", err)
+		}
+
+		if exist < 1 {
+			_, err := repo.AddCategoryGroup(group)
+			if err != nil {
+				log.Fatalf("Failed adding category group: %v", err)
+			}
+		}
+	}
+
+	collegeGroupID := GetGroupID(repo, "College")
+	workGroupID := GetGroupID(repo, "Work")
+	communityGroupID := GetGroupID(repo, "Community")
+
 	categories := []sqlc.AddCategoryParams{
-		{ID: uuid.New().String(), CategoryName: "Product Design"},
-		{ID: uuid.New().String(), CategoryName: "Product Management"},
-		{ID: uuid.New().String(), CategoryName: "Front-End Development"},
-		{ID: uuid.New().String(), CategoryName: "Back-End Development"},
-		{ID: uuid.New().String(), CategoryName: "Data Science"},
+		{ID: uuid.New().String(), CategoryName: "Academic Societies", GroupID: collegeGroupID},
+		{ID: uuid.New().String(), CategoryName: "Self-Development Organizations", GroupID: collegeGroupID},
+		{ID: uuid.New().String(), CategoryName: "Cultural Organizations", GroupID: collegeGroupID},
+		{ID: uuid.New().String(), CategoryName: "Recreational/Sports Clubs", GroupID: collegeGroupID},
+		{ID: uuid.New().String(), CategoryName: "Professional Organizations", GroupID: collegeGroupID},
+		{ID: uuid.New().String(), CategoryName: "Service Organizations", GroupID: collegeGroupID},
+		{ID: uuid.New().String(), CategoryName: "Student Government", GroupID: collegeGroupID},
+		{ID: uuid.New().String(), CategoryName: "Artistic/Music Groups", GroupID: collegeGroupID},
+		{ID: uuid.New().String(), CategoryName: "Social Organizations", GroupID: collegeGroupID},
+		{ID: uuid.New().String(), CategoryName: "Environmental Organizations", GroupID: collegeGroupID},
+		{ID: uuid.New().String(), CategoryName: "Religious/Spiritual Organizations", GroupID: collegeGroupID},
+		{ID: uuid.New().String(), CategoryName: "Political Organizations", GroupID: collegeGroupID},
+		{ID: uuid.New().String(), CategoryName: "Health and Wellness Groups", GroupID: collegeGroupID},
+
+		{ID: uuid.New().String(), CategoryName: "Professional Development Organizations", GroupID: workGroupID},
+		{ID: uuid.New().String(), CategoryName: "Employee Resource Groups (ERGs)", GroupID: workGroupID},
+		{ID: uuid.New().String(), CategoryName: "Wellness Committees", GroupID: workGroupID},
+		{ID: uuid.New().String(), CategoryName: "Social Committees", GroupID: workGroupID},
+		{ID: uuid.New().String(), CategoryName: "Innovation Hubs", GroupID: workGroupID},
+		{ID: uuid.New().String(), CategoryName: "Sustainability Committees", GroupID: workGroupID},
+		{ID: uuid.New().String(), CategoryName: "Union/Workers Councils", GroupID: workGroupID},
+		{ID: uuid.New().String(), CategoryName: "Charity/Community Outreach Groups", GroupID: workGroupID},
+		{ID: uuid.New().String(), CategoryName: "Networking Groups", GroupID: workGroupID},
+		{ID: uuid.New().String(), CategoryName: "Mentorship Programs", GroupID: workGroupID},
+
+		{ID: uuid.New().String(), CategoryName: "Self-Development Groups", GroupID: communityGroupID},
+		{ID: uuid.New().String(), CategoryName: "Social Clubs", GroupID: communityGroupID},
+		{ID: uuid.New().String(), CategoryName: "Recreational/Sports Leagues", GroupID: communityGroupID},
+		{ID: uuid.New().String(), CategoryName: "Environmental Groups", GroupID: communityGroupID},
+		{ID: uuid.New().String(), CategoryName: "Volunteer/Service Organizations", GroupID: communityGroupID},
+		{ID: uuid.New().String(), CategoryName: "Political Action Groups", GroupID: communityGroupID},
+		{ID: uuid.New().String(), CategoryName: "Cultural Groups", GroupID: communityGroupID},
+		{ID: uuid.New().String(), CategoryName: "Artistic/Music Groups", GroupID: communityGroupID},
+		{ID: uuid.New().String(), CategoryName: "Business Networks", GroupID: communityGroupID},
+		{ID: uuid.New().String(), CategoryName: "Health and Wellness Groups", GroupID: communityGroupID},
+		{ID: uuid.New().String(), CategoryName: "Educational Organizations", GroupID: communityGroupID},
 	}
 
 	for _, category := range categories {
-		_, err = repo.AddCategory(category)
+		exist, err := repo.CategoryExists(sqlc.CategoryExistsParams{
+			CategoryName: category.CategoryName,
+			GroupID:      category.GroupID,
+		})
 		if err != nil {
-			log.Fatalf("Failed adding category: %v", err)
+			log.Fatalf("Failed checking category: %v", err)
+		}
+
+		if exist < 1 {
+			_, err = repo.AddCategory(category)
+			if err != nil {
+				log.Fatalf("Failed adding category: %v", err)
+			}
 		}
 	}
+}
+
+func GetGroupID(r repository.SeederRepository, s string) int32 {
+	id, err := r.GetGroupIDByName(s)
+	if err != nil {
+		log.Fatalf("Failed getting category group: %v", err)
+	}
+	return id
 }
